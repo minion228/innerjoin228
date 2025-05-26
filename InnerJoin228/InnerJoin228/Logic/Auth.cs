d7e09e75d406263df6ef462dfdb9bad8e2597315
minion228 <yuliapr1984@gmail.com>
2025-05-26 18:27:59 +0300

    1

---
+++ InnerJoin228/InnerJoin228/Logic/Auth.cs
@@ -0,0 +1,28 @@
+﻿using InnerJoin228.ModelDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using InnerJoin228.Classes;
+using System.Threading.Tasks;
+using InnerJoin228.Interfaces;
+
+namespace InnerJoin228.Logic
+{
+    public class Auth:IAuth
+    {
+        InnerJoin228Entities db=new InnerJoin228Entities();
+        public bool Check(string login, string password)
+        {
+            var user = db.Users.Include(u=>u.Roles).FirstOrDefault(u=>u.UserLogin==login && u.UserPassword==password);
+            if (user!=null)
+            {
+                
+                new Profile(user).Show();
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ InnerJoin228/InnerJoin228/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using InnerJoin228.Interfaces;
+using InnerJoin228.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InnerJoin228
+{
+
+    public partial class MainWindow : Window
+    {
+        private IAuth _auth;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _auth= new Auth();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string login=tbxLogin.Text;
+            string password=tbxPassword.Password;
+            if (_auth.Check(login, password))
+            {
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль","");
+                tbxLogin.Clear();
+                tbxPassword.Clear();
+            }
+        }
+    }
+}
